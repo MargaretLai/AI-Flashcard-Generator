0a893d353122c5220310ec0a16f8980a98e8b666
MargaretLai <MargaretLUI01@gmail.com>
2024-08-18 19:39:00 -0400

    Protected functional routes via Clerk

---
+++ app/generate/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { getAuth } from '@clerk/nextjs/server'
+
+export default function page() {
+  return (
+    <div>
+      This is the functional page
+    </div>
+  )
+}
